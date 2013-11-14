53b33ed2a89057fa401abfa3f0d8cd5919e40103
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-11-14 14:53:12 +0100

    start the migration to shapeless 2.0

--- src/main/scala/MapFoldSets.scala
+++ src/main/scala/MapFoldSets.scala
@@ -6,6 +6,8 @@ Just a copy of MapFolder for `HList`s from shapeless.
 
 package ohnosequences.typesets
 
+import shapeless.poly.Case
+
 trait SetMapFolder[S <: TypeSet, R, F] {
   def apply(s: S, in: R, op: (R, R) => R): R 
 }
@@ -19,7 +21,7 @@ object SetMapFolder {
   }
   
   implicit def cons[H, T <: TypeSet, R, F <: Poly]
-    (implicit hc: Pullback1Aux[F, H, R], tf: SetMapFolder[T, R, F]) =
+    (implicit hc: Case.Aux[F, H, R], tf: SetMapFolder[T, R, F]) =
       new SetMapFolder[H :+: T, R, F] {
           def apply(s: H :+: T, in: R, op: (R, R) => R) = op(hc(s.head), tf(s.tail, in, op))
       }
