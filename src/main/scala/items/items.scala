57970606ffa9c9e06f751caa205b3c0383610189
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-11-15 18:04:43 +0100

    try to split ops into api and impl: failed, clean up a bit

--- src/main/scala/items/items.scala
+++ src/main/scala/items/items.scala
@@ -9,92 +9,95 @@ import ohnosequences.typesets._
 //    3. for providing Ops, you need Syntax
 //    4. and the carrier bounds a particular GADT "shape"
 // 
+// I want to check whether is possible to abstract over the type for which syntax
+// is provided. In this case, FieldType? FieldOf?
 object experiment {
 
-  trait OpsCarrier {
+  trait AnyCarrier {
 
     type GADT
     type Cons[H, T <: GADT] <: GADT { val head: H; val tail: T  }
     type Nil <: GADT
   }
 
-  
-
-  object AnyItemOps {
-    type using[C <: OpsCarrier] = AnyItemOps {  type Carrier = C  }
+  object AnyOps {
+    type using[C <: AnyCarrier] = AnyOps {  type Carrier = C  }
   }
-  trait AnyItemOps { type Carrier <: OpsCarrier 
-    val c: Carrier
+  trait AnyOps { 
 
-    import shapeless.record._
-    import scala.language.reflectiveCalls
+    type Carrier <: AnyCarrier 
+    val carrier: Carrier
 
+    // this is really ops, the rest is ops implementation
     @annotation.implicitNotFound(msg = "No field ${K} in record ${S}")
-    trait Selector[S <: c.GADT, K] {
+    trait Selector[S <:  carrier.GADT, K] {
       type Out
       def apply(l: S): Out
     }
 
-    trait LowPrioritySelector {
-    type Aux[S <: c.GADT, K, Out0] = Selector[S, K] { type Out = Out0 }
+    import shapeless.record._
+    import scala.language.reflectiveCalls
 
-    implicit def setSelect[H, T <: c.GADT, K]
-      (implicit st : Selector[T, K]): Aux[c.Cons[H,T], K, st.Out] =
-        new Selector[c.Cons[H,T], K] {
+    trait LowPrioritySelector {
+    type Aux[S <: carrier.GADT, K, Out0] = Selector[S, K] { type Out = Out0 }
+
+    implicit def setSelect[H, T <:  carrier.GADT, K]
+      (implicit st : Selector[T, K]): Aux[ carrier.Cons[H,T], K, st.Out] =
+        new Selector[ carrier.Cons[H,T], K] {
           type Out = st.Out
-          def apply(l : c.Cons[H,T]): Out = st(l.tail)
+          def apply(l :  carrier.Cons[H,T]): Out = st(l.tail)
         }
     }
 
     object Selector extends LowPrioritySelector {
-      def apply[S <: c.GADT, K](implicit selector: Selector[S, K]): Aux[S, K, selector.Out] = selector
+      def apply[S <:  carrier.GADT, K](implicit selector: Selector[S, K]): Aux[S, K, selector.Out] = selector
 
-      implicit def setSelect1[K, V, T <: c.GADT]: Aux[c.Cons[FieldType[K,V],T], K, V] =
-        new Selector[c.Cons[FieldType[K, V], T], K] {
+      implicit def setSelect1[K, V, T <:  carrier.GADT]: Aux[ carrier.Cons[FieldType[K,V],T], K, V] =
+        new Selector[ carrier.Cons[FieldType[K, V], T], K] {
           type Out = V
-          def apply(l: c.Cons[FieldType[K, V],T]): Out = l.head
+          def apply(l:  carrier.Cons[FieldType[K, V],T]): Out = l.head
         }
     }
   }
 
-  class ItemOps[C <: OpsCarrier](val c: C) extends AnyItemOps {
+  class Ops[C <: AnyCarrier](val carrier: C) extends AnyOps {
     type Carrier = C
   }
 
-  trait AnyItemSyntax {
+  trait AnySyntax {
 
-    type Ops <: AnyItemOps
+    type Ops <: AnyOps
     val ops: Ops
-    type GADT = ops.c.GADT
-    implicit def itemOps[S <: GADT](s: S): Syntax[S] = new Syntax(s)
+    type GADT = ops.carrier.GADT
+    implicit def itemOps[S <: GADT](s: S): API[S] = new API(s)
 
-    final class Syntax[S <: GADT](val l: S) {
+    final class API[S <: GADT](val l: S) {
       import shapeless.Witness
       import shapeless.syntax.singleton._
       import ops._
       def get(k: Witness)(implicit selector: ops.Selector[S, k.T]): selector.Out = selector(l)
     }
   }
-  class ItemSyntax[O <: AnyItemOps](val ops: O) extends AnyItemSyntax { type Ops = O }
+  class Syntax[O <: AnyOps](val ops: O) extends AnySyntax { type Ops = O }
 
   // now let's implement this
-  case object TypeSetGADT extends OpsCarrier {
+  case object TypeSetCarrier extends AnyCarrier {
     type GADT = TypeSet
     type Cons[H, T <: GADT] = :~:[H,T]
     type Nil = ∅
   }
-  case object TypeSetItemOps extends ItemOps(TypeSetGADT)
-  case object TypeSetSyntax extends ItemSyntax(TypeSetItemOps)
+  case object TypeSetOps extends Ops(TypeSetCarrier)
+  case object TypeSetSyntax extends Syntax(TypeSetOps)
 
   // now shapeless
-  case object HListGADT extends OpsCarrier {
+  case object HListCarrier extends AnyCarrier {
     import shapeless._
     type GADT = HList
     type Cons[H, T <: GADT] = ::[H,T]
     type Nil = HNil
   }
-  case object HListItemOps extends ItemOps(HListGADT)
-  case object HListSyntax extends ItemSyntax(HListItemOps)
+  case object HListOps extends Ops(HListCarrier)
+  case object HListSyntax extends Syntax(HListOps)
 
 }
 
