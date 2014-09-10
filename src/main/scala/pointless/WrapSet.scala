f81ef7ef0daba28763d0ba9e68808c50e92b4819
Alexey Alekhin <laughedelic@gmail.com>
2014-09-11 00:45:38 +0200

    Added WrapSet as a specialized version of TypeSet of Wraps; Changed record to a wrapper for this; With fixed parser everything works without any additional changes

---
+++ src/main/scala/pointless/WrapSet.scala
@@ -0,0 +1,65 @@
+package ohnosequences.pointless
+
+import AnyTypeSet._, AnyProperty._, AnyFn._, AnyTypeUnion._
+
+trait AnyWrapSet extends AnyTypeSet with AnyWrap {
+  override def toString = "<" + toStr + ">"
+
+  // type Bound <: just[AnyWrap]
+  type Raw <: AnyTypeSet
+}
+
+trait EmptyWrapSet extends AnyWrapSet {
+  def toStr = "-"
+
+  type Types = TypeUnion.empty
+  type Bound = Types#union
+  type Raw = ∅
+}
+
+object EmptyWrapSet extends EmptyWrapSet
+
+
+trait NonEmptyWrapSet extends NonEmptySet with AnyWrapSet {
+
+  type Head <: AnyWrap
+  type Tail <: AnyWrapSet
+
+  type Types = TypesOf[Tail]#or[Head]
+  type Bound = Types#union
+
+  type Raw = ValueOf[Head] :~: Tail#Raw
+}
+
+case class ConsWrapSet[H <: AnyWrap, T <: AnyWrapSet]
+  (val head : H,  val tail : T)(implicit val headIsNew: H ∉ T) extends NonEmptyWrapSet {
+  type Head = H; type Tail = T
+
+  def toStr = head.toString + ", " + tail.toStr
+}
+
+/* This method covers constructor to check that you are not adding a duplicate */
+object ConsWrapSet {
+  def cons[H <: AnyWrap, T <: AnyWrapSet](h: H, set: T)(implicit check: H ∉ T): ConsWrapSet[H,T] = ConsWrapSet(h, set) 
+}
+
+object AnyWrapSet {
+
+  final type ⦲ = EmptyWrapSet
+  val ⦲ : ⦲ = EmptyWrapSet
+
+  final type :^:[H <: AnyWrap, T <: AnyWrapSet] = ConsWrapSet[H, T]
+
+  type Of[T] = AnyWrapSet { type Bound <: just[T] }
+
+  implicit def wrapSetOps[W <: AnyWrapSet](w: W): WrapSetOps[W] = new WrapSetOps[W](w)
+}
+
+class WrapSetOps[W <: AnyWrapSet](w: W) {
+  import AnyWrapSet._
+
+  def :^:[H <: AnyWrap](h: H)(implicit check: H ∉ W): (H :^: W) = ConsWrapSet.cons(h, w)(check)
+
+  import ops.typeSet.ParseFrom
+  def parseFrom[X](x: X)(implicit parser: W ParseFrom X): parser.Out = parser(w, x)
+}
