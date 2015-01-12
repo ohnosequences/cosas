65cb75c124fd777aa8b6a2725f6669b4f1fb5d65
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-01-12 16:32:32 +0100

    rename things, no more iso#out trick, make everything a test

--- src/main/scala/cosas/equality.scala
+++ src/main/scala/cosas/equality.scala
@@ -1,8 +1,7 @@
 package ohnosequences.cosas
 
-object equality extends LowPrior {
+object equals {
   
-
   @annotation.implicitNotFound( msg = 
   """
   No proof of equality found for types:  
@@ -11,49 +10,30 @@ object equality extends LowPrior {
 
     ${B}
   """)
-  trait Equality[A, B] { 
+  sealed trait ≃[A, B] { 
 
+    type Left = A
+    type Right = B
     type Out >: A with B <: A with B 
 
     implicit def inL(a: A): Out
     implicit def inR(b: B): Out
 
-    final implicit def left(o: Out): A = o
-    final implicit def right(o: Out): B = o
+    final implicit def elimL(o: Out): A = o
+    final implicit def elimR(o: Out): B = o
   }
-  type EqualityContext[X, Y] = (X,Y)
-  type ?≃[A, B] = EqualityContext[A,B] => (A Equality B)
-  type ≃[A,B] = Equality[A,B]#Out
-  final class Refl[A] extends (A Equality A) { 
+
+  final case class Refl[A]() extends (A ≃ A) { 
 
     type Out = A
 
-    implicit final def swap(a: A): A = a
-    implicit final def inL(a: A): Out = a
-    implicit final def inR(b: A): Out = b
+    final implicit def inL(a: A): Out = a
+    final implicit def inR(b: A): Out = b
   } 
-  implicit def refl[A >: B <: B, B]: 
-    (EqualityContext[A,B] => A Equality B) = x => new Refl[B]
 
-  implicit def reflEq[B]: B Equality B = new Refl[B]
-  implicit def kindEq[F[_], A](implicit p: A Equality A): F[A] Equality F[A] = new Refl[F[A]]
-  
-  // implicit def sym[A,B](implicit eqWitness: Equality)
-  // implicit def coerce2[A,B](b: B)(implicit eqWitness: A ?≃ B): A ≃ B = b
+  type ?≃[X, Y] = Either[X,Y]
+  type <≃>[A, B] = (A ?≃ B) => (A ≃ B)
 
-  object coercion {
-
-    implicit def coerceL[A, B](a: A)(implicit eqWitness: A ?≃ B): A ≃ B = a
-    implicit def coerceR[A, B](a: A)(implicit eqWitness: B ?≃ A): B ≃ A = a
-  }
-
+  implicit def refl[A >: B <: B, B]: (A <≃> B) = x => Refl[B]()
+  implicit def reflInst[B]: B ≃ B = Refl[B]()
 }
-
-trait LowPrior {
-
-    import equality._
-
-    implicit def refl2[A >: B <: B, B]: (EqualityContext[A,B] => B Equality A) = x => new Refl[A]
-
-    implicit def sym[A,B >: A <: A, X <: ?≃[A, B]]: (B ?≃ A) = refl[B,A]
-  }
--- src/test/scala/cosas/EqualityTests.scala
+++ src/test/scala/cosas/EqualityTests.scala
@@ -1,68 +1,97 @@
 package ohnosequences.scala.tests
 
-import ohnosequences.cosas._, equality._
+import ohnosequences.cosas._, equals._
 
-object eqStuff {
-
-  type string = String
-  type stringAgain = String
-  import coercion._  
-  
-  implicitly[string ?≃ stringAgain]
-  implicitly[stringAgain ?≃ string]
-  implicitly[string ?≃ string]
-  implicitly[stringAgain ?≃ stringAgain]
-  
-  def doSomething[X,Y](x: X, y: Y)(implicit ev: X ?≃ Y): (X ≃ Y) = y
-  def doSomethingDifferently[X,Y](x: X, y: Y)(implicit ev: X ?≃ Y): (X ≃ Y) = x
-  def doSomethingElse[X,Y](x: X, y: Y)(implicit ev: Y ?≃ X): (Y ≃ X) = x
-
-  val x: String = doSomething("buh", "boh")
-
-  // I cannot remove the return type (cyclic blahblah); why?
-  def uh: String = doSomethingElse(doSomething(doSomethingDifferently("buh","boh"), doSomething("boh","buh")), "oh")
-
-
-  final case class XList[A](xs: List[A]) {
-
-    def sum(implicit ev: List[Int] Equality List[A]): Int = {
-
-      import ev._
-
-      (xs:List[Int]).foldLeft(0)(_ + _)
-    }
-
-    def sum2(implicit ev: List[A] Equality List[Int]): Int = {
-
-      import ev._
-
-      (xs:List[Int]).foldLeft(0)(_ + _)
-    }
-      
-  }
-
-  val xl = XList(List(1,2))
-
-  val z = xl.sum
-}
-
-final class EqualityTests extends org.scalatest.FunSuite {
+final class EqualsTests extends org.scalatest.FunSuite {
 
   test("basic type equality") {
 
     type string = String
     type stringAgain = String
-    import coercion._  
+    type lstring = List[String]
+    type llstring = List[List[String]]
+    type llstringAgain = List[lstring]
+    // import coercion._  
     
-    implicitly[string ?≃ stringAgain]
-    implicitly[stringAgain ?≃ string]
-    implicitly[string ?≃ string]
-    implicitly[stringAgain ?≃ stringAgain]
-    
-    def doSomething[X,Y](x: X, y: Y)(implicit ev: X ?≃ Y): (X ≃ Y) = y
-    def doSomethingDifferently[X,Y](x: X, y: Y)(implicit ev: X ?≃ Y): (X ≃ Y) = x
+    implicitly[string <≃> stringAgain]
+    implicitly[stringAgain <≃> string]
+    implicitly[string <≃> string]
+    implicitly[stringAgain <≃> stringAgain]
 
-    // this fails here, due to some scalatest thing?
-    // val x: String = doSomething("buh", "boh")
+    implicitly[lstring <≃> List[String]]
+    implicitly[List[String] <≃> lstring]
+    implicitly[llstring <≃> llstringAgain]
+    implicitly[llstringAgain <≃> llstring]
+    implicitly[List[llstringAgain] <≃> List[List[List[String]]]]
+    implicitly[Map[String,Boolean] <≃> Map[String,Boolean] ]
+    implicitly[Map[_,_] <≃> Map[_,_]]
+    object two
+    val y = two
+    implicitly[two.type <≃> y.type]
+
+    def doSomething[X,Y](x: X, y: Y)(implicit id: X ≃ Y): Y = id.inL(x)
+    def doSomethingDifferently[X,Y](x: X, y: Y)(implicit id: X <≃> Y): X = x
+    def doSomethingElse[X,Y](x: X, y: Y)(implicit ev: Y <≃> X): Y = y
+
+    val x: String = doSomething("buh", "boh")
+    def uh = doSomethingElse(doSomething(doSomethingDifferently("buh","boh"), doSomething("boh","buh")), "oh")
+
+    final case class XList[A](xs: List[A]) {
+
+      def sum(implicit ev: List[Int] ≃ List[A]): Int = {
+
+        import ev._
+        (xs:List[Int]).foldLeft(0)(_ + _)
+      }
+
+      def sum2(implicit ev: List[A] ≃ List[Int]): Int = {
+
+        import ev._
+        (xs:List[Int]).foldLeft(0)(_ + _)
+      }      
+    }
+
+    val xl = XList(List(1,2))
+    val z = xl.sum
+    val z2 = xl.sum2
+
+    assert(z === z2)
+
+    object gadt {
+
+      object AnyFoo {
+
+        type Foo[A0,B0] = AnyFoo { type A = A0; type B = B0 }
+      }
+      sealed trait AnyFoo {
+
+        type A
+        type B
+      }
+
+      final case class X[A0]() extends AnyFoo {
+
+        type A = A0
+        type B = A
+      }
+
+      final case class Y[A0, B0](a: A0, b: B0) extends AnyFoo {
+
+        type A = A0
+        type B = B0
+      }
+
+      import AnyFoo.Foo
+
+      def hoge1[F[_, _], A, B, C](foo: Foo[A,B], bar: F[A, C]): F[B, C] = foo match {
+
+        case X() => bar
+      }
+
+      val x = X[Int]
+
+      val z = hoge1(x, Y(123, "hola")) 
+      assert(z === "hola")
+    }
   }
 }
