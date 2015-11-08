b8ee2d0a0f9e9ff0d39aa9beccbafcae7ade4943
Alexey Alekhin <laughedelic@gmail.com>
2015-11-08 20:05:05 +0100

    Uncommented/ignored the rest of records tests

--- src/test/scala/cosas/RecordTests.scala
+++ src/test/scala/cosas/RecordTests.scala
@@ -127,140 +127,107 @@ class RecordTypeTests extends org.scalatest.FunSuite {
     // assert { simpleUserEntry === ( normalUserEntry as simpleUser ) }
   }
 
-//   test("can provide properties in different order") {
-//
-//     // the declared property order
-//     implicitly [
-//       simpleUser.Raw =:= (ValueOf[id.type] :: ValueOf[name.type] :: KNil[AnyDenotation])
-//     ]
-//
-//     // they get reordered
-//     val simpleUserV: ValueOf[simpleUser.type] = simpleUser {
-//       id(29681)       ::
-//       name("Antonio") :: KNil[AnyDenotation]
-//     }
-//
-//     val sameSimpleUserV: ValueOf[simpleUser.type] = simpleUser {
-//       id(29681)       ::
-//       name("Antonio") :: KNil[AnyDenotation]
-//     }
-//
-//     assert { simpleUserV == sameSimpleUserV }
-//   }
-//   //
-//   // test("can check if record has properties") {
-//   //
-//   //   implicitly[simpleUser.SetOfTypes HasProperties (id.type :: name.type :: KNil[AnyDenotation])]
-//   //   implicitly[simpleUser.SetOfTypes HasProperties (name.type :: id.type :: KNil)]
-//   //   implicitly[simpleUser.SetOfTypes HasProperties (name.type :: KNil)]
-//   //   implicitly[simpleUser.SetOfTypes HasProperties (id.type :: KNil)]
-//   //
-//   //   implicitly[simpleUser.SetOfTypes HasProperty name.type]
-//   //   implicitly[simpleUser.SetOfTypes HasProperty id.type]
-//   //
-//   //   assertTypeError { """implicitly[simpleUser.SetOfTypes HasProperties (email.type :: id.type :: KNil)]""" }
-//   //   assertTypeError { """implicitly[simpleUser.SetOfTypes HasProperties (email.type :: name.type :: color.type :: KNil)]""" }
-//   // }
-// //
-//   case object propertyConverters {
-//
-//     val idVParser: String => Option[Int] = str => {
-//       import scala.util.control.Exception._
-//       catching(classOf[NumberFormatException]) opt str.toInt
-//     }
-//     implicit val idParser   = PropertyParser(id, id.label)(idVParser)
-//     implicit val nameParser = PropertyParser(name, name.label){ str: String => Some(str) }
-//
-//     implicit val idSerializer   = PropertySerializer(id, id.label)( x => Some(x.toString) )
-//     implicit val nameSerializer = PropertySerializer(name, name.label){ x: String => Some(x) }
-//   }
-//
-//   test("can parse records from Maps") {
-//
-//     import propertyConverters._
-//
-//     val simpleUserEntryMap =  Map(
-//       "id" -> "29681",
-//       "name" -> "Antonio"
-//     )
-//     val wrongKeyMap = Map(
-//       "idd" -> "29681",
-//       "name" -> "Antonio"
-//     )
-//
-//     val notIntValueMap = Map(
-//       "name" -> "Antonio",
-//       "id" -> "twenty-two"
-//     )
-//
-//     val mapWithOtherStuff = simpleUserEntryMap + ("other" -> "stuff")
-//
-//     assert {
-//       parseDenotations[String,simpleUser.Raw](simpleUserEntryMap) ===
-//       Right(id(29681) :: name("Antonio") :: KNil[AnyDenotation])
-//     }
-//     assert {
-//       parseDenotations[String,simpleUser.Raw](wrongKeyMap) ===
-//       Left(KeyNotFound(id.label, wrongKeyMap))
-//     }
-//     assert {
-//       parseDenotations[String,simpleUser.Raw](notIntValueMap) ===
-//       Left(ErrorParsing(ErrorParsingValue(id)("twenty-two")))
-//     }
-//     assert {
-//       parseDenotations[String,simpleUser.Raw](mapWithOtherStuff) ===
-//       Right(id(29681) :: name("Antonio") :: KNil[AnyDenotation])
-//     }
-//   }
-//
-//   test("can serialize records to Maps") {
-//
-//     import propertyConverters._
-//
-//     val simpleUserEntryMap =  Map(
-//       "id" -> "29681",
-//       "name" -> "Antonio"
-//     )
-//     assert {
-//       Right(simpleUserEntryMap) ===
-//       serializeDenotations[String,simpleUser.Raw](Map[String,String](), id(29681) :: name("Antonio") :: KNil[AnyDenotation])
-//     }
-//
-//     val unrelatedMap = Map(
-//       "lala" -> "hola!",
-//       "ohno" -> "pigeons"
-//     )
-//
-//     val mapWithKey = unrelatedMap + ("id" -> "1321")
-//
-//     assert {
-//       Right(simpleUserEntryMap ++ unrelatedMap) ===
-//       serializeDenotations[String,simpleUser.Raw](
-//         unrelatedMap,
-//         id(29681) :: name("Antonio") :: KNil[AnyDenotation]
-//       )
-//     }
-//
-//     assert {
-//       Left(KeyPresent(id.label, mapWithKey)) ===
-//       serializeDenotations[String,simpleUser.Raw](
-//         mapWithKey,
-//         id(29681) :: name("Antonio") :: KNil[AnyDenotation]
-//       )
-//     }
-//   }
-//
-//   // test("can get values from records as lists and typesets") {
-//   //
-//   //   val vRecordTypeEntryValues = mapToListOf[String](
-//   //     denotationValue,
-//   //     // need to add the type here
-//   //     volatileRecEntry.value: ValueOf[email.type] :: ValueOf[color.type] :: KNil[AnyDenotation]
-//   //   )
-//   //
-//   //   val simpleUserValues: Int :: String :: KNil[Any] = simpleUserEntry.value map denotationValue
-//   //   val simpleUserValuesAny = mapToListOf[Any](denotationValue, simpleUserEntry.value)
-//   //
-//   //   assert { simpleUserValuesAny === simpleUserValues.toList[Any] }
-//   // }
+
+  ignore("can check if record has properties") {
+
+    // implicitly[simpleUser.SetOfTypes HasProperties (id.type :: name.type :: KNil[AnyDenotation])]
+    // implicitly[simpleUser.SetOfTypes HasProperties (name.type :: id.type :: KNil)]
+    // implicitly[simpleUser.SetOfTypes HasProperties (name.type :: KNil)]
+    // implicitly[simpleUser.SetOfTypes HasProperties (id.type :: KNil)]
+    //
+    // implicitly[simpleUser.SetOfTypes HasProperty name.type]
+    // implicitly[simpleUser.SetOfTypes HasProperty id.type]
+    //
+    // assertTypeError { """implicitly[simpleUser.SetOfTypes HasProperties (email.type :: id.type :: KNil)]""" }
+    // assertTypeError { """implicitly[simpleUser.SetOfTypes HasProperties (email.type :: name.type :: color.type :: KNil)]""" }
+  }
+
+  // case object propertyConverters {
+  //
+  //   val idVParser: String => Option[Int] = str => {
+  //     import scala.util.control.Exception._
+  //     catching(classOf[NumberFormatException]) opt str.toInt
+  //   }
+  //   implicit val idParser   = PropertyParser(id, id.label)(idVParser)
+  //   implicit val nameParser = PropertyParser(name, name.label){ str: String => Some(str) }
+  //
+  //   implicit val idSerializer   = PropertySerializer(id, id.label)( x => Some(x.toString) )
+  //   implicit val nameSerializer = PropertySerializer(name, name.label){ x: String => Some(x) }
+  // }
+
+  ignore("can parse records from Maps") {
+
+    // import propertyConverters._
+    //
+    // val simpleUserEntryMap =  Map(
+    //   "id" -> "29681",
+    //   "name" -> "Antonio"
+    // )
+    // val wrongKeyMap = Map(
+    //   "idd" -> "29681",
+    //   "name" -> "Antonio"
+    // )
+    //
+    // val notIntValueMap = Map(
+    //   "name" -> "Antonio",
+    //   "id" -> "twenty-two"
+    // )
+    //
+    // val mapWithOtherStuff = simpleUserEntryMap + ("other" -> "stuff")
+    //
+    // assert {
+    //   parseDenotations[String,simpleUser.Raw](simpleUserEntryMap) ===
+    //   Right(id(29681) :: name("Antonio") :: KNil[AnyDenotation])
+    // }
+    // assert {
+    //   parseDenotations[String,simpleUser.Raw](wrongKeyMap) ===
+    //   Left(KeyNotFound(id.label, wrongKeyMap))
+    // }
+    // assert {
+    //   parseDenotations[String,simpleUser.Raw](notIntValueMap) ===
+    //   Left(ErrorParsing(ErrorParsingValue(id)("twenty-two")))
+    // }
+    // assert {
+    //   parseDenotations[String,simpleUser.Raw](mapWithOtherStuff) ===
+    //   Right(id(29681) :: name("Antonio") :: KNil[AnyDenotation])
+    // }
+  }
+
+  ignore("can serialize records to Maps") {
+
+    // import propertyConverters._
+    //
+    // val simpleUserEntryMap =  Map(
+    //   "id" -> "29681",
+    //   "name" -> "Antonio"
+    // )
+    // assert {
+    //   Right(simpleUserEntryMap) ===
+    //   serializeDenotations[String,simpleUser.Raw](Map[String,String](), id(29681) :: name("Antonio") :: KNil[AnyDenotation])
+    // }
+    //
+    // val unrelatedMap = Map(
+    //   "lala" -> "hola!",
+    //   "ohno" -> "pigeons"
+    // )
+    //
+    // val mapWithKey = unrelatedMap + ("id" -> "1321")
+    //
+    // assert {
+    //   Right(simpleUserEntryMap ++ unrelatedMap) ===
+    //   serializeDenotations[String,simpleUser.Raw](
+    //     unrelatedMap,
+    //     id(29681) :: name("Antonio") :: KNil[AnyDenotation]
+    //   )
+    // }
+    //
+    // assert {
+    //   Left(KeyPresent(id.label, mapWithKey)) ===
+    //   serializeDenotations[String,simpleUser.Raw](
+    //     mapWithKey,
+    //     id(29681) :: name("Antonio") :: KNil[AnyDenotation]
+    //   )
+    // }
+  }
+
 }
