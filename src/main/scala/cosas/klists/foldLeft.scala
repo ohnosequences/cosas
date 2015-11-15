666170c207ea3ee8faed2639dc96ed08218678b9
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-11-15 17:34:11 +0100

    change foldLeft implicits to use simpler types

--- src/main/scala/cosas/klists/foldLeft.scala
+++ src/main/scala/cosas/klists/foldLeft.scala
@@ -3,7 +3,7 @@ package ohnosequences.cosas.klists
 import ohnosequences.cosas.fns._
 
 // TODO review this signature
-class FoldLeft[L <: AnyKList, F <: AnyDepFn2, Z <: F#Out] extends DepFn3[
+class FoldLeft[L <: AnyKList, F <: AnyDepFn2, Z] extends DepFn3[
   L, Z, F,
   F#Out
 ]
@@ -21,32 +21,27 @@ case object FoldLeft {
 
   implicit def cons[
     F <: AnyDepFn2 {
-      type In1 = B; type In2 = A; type Out = B
+      type In1 >: O; type In2 >: H; type Out >: FO
     },
-    // head cons
-    H <: A, T <: AnyKList { type Bound = A },
-    // zero
-    Z <: B,
+    H <: T#Bound, T <: AnyKList,
+    Z,
     O,
-    A,
-    B >: O
+    FO
   ](implicit
-    evF: AnyApp2At[F, O, H],
+    evF: AnyApp2At[F, O, H] { type Y = FO },
     foldLeft: AnyApp3At[FoldLeft[T,F,Z], T, Z, F] { type Y = O }
   )
-  : App3[
+  : AnyApp3At[
       FoldLeft[H :: T,F,Z],
       H :: T,
-      Z,F,
-      evF.Y
-    ] =
+      Z,F
+    ] { type Y = FO }=
     App3 {
       (xs: H :: T, z: Z, f: F) => {
 
         val uh: H = xs.head
         val oh = foldLeft(xs.tail,z,f)
         evF(oh,uh)
-
       }
     }
 }
