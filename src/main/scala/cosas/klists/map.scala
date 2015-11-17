d8a1ed91fb165bee51252c2ca213d50a0dfd75bb
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-11-17 18:41:10 +0100

    change map to a depfn2 with better signature

--- src/main/scala/cosas/klists/map.scala
+++ src/main/scala/cosas/klists/map.scala
@@ -3,39 +3,57 @@ package ohnosequences.cosas.klists
 import ohnosequences.cosas._, fns._
 
 // TODO signature as in fold
-class MapKListOf[
-  F <: AnyDepFn1,
-  Y
-]
-extends DepFn1[
+class MapKListOf[F <: AnyDepFn1, Y <: F#Out] extends DepFn2[
+  F,
   AnyKList,
   AnyKList { type Bound = Y }
 ]
 
-case object MapKListOf {
+case object MapKListOf extends WithSameType {
 
   implicit def empty[
-    F <: AnyDepFn1,
+    F <: AnyDepFn1 { type Out >: B },
     A,
-    B0
+    B
   ]
-  : AnyApp1At[MapKListOf[F,B0],KNil[A]] { type Y = KNil[B0] } =
-    App1 { (e: KNil[A]) => KNil[B0] }
+  : AnyApp2At[MapKListOf[F,B], F, KNil[A]] { type Y = KNil[B] } =
+    App2 { (f: F, e: KNil[A]) => KNil[B] }
+
+  implicit def kconsBis[
+    F <: AnyDepFn1 { type Out >: U },
+    H <: InT#Bound, InT <: AnyKList { type Bound <: F#In1 },
+    U >: O,
+    O
+  ](implicit
+    evF: AnyApp1At[F,H] { type Y = O },
+    mapof: AnyApp2At[
+      MapKListOf[F,U],
+      F,
+      InT
+    ]
+  )
+  : AnyApp2At[MapKListOf[F,U], F, H :: InT] { type Y = O :: mapof.Y } =
+    App2[MapKListOf[F,U], F, H :: InT, O :: mapof.Y] {
+      (f: F, s: H :: InT) => evF(s.head) :: mapof(f,s.tail)
+    }
+}
+
+trait WithSameType {
 
   implicit def kcons[
     F <: AnyDepFn1 { type Out >: O },
     H <: InT#Bound, InT <: AnyKList { type Bound <: F#In1 },
-    Y >: O,
     O
   ](implicit
     evF: AnyApp1At[F,H] { type Y = O },
-    mapof: AnyApp1At[
-      MapKListOf[F,Y],
+    mapof: AnyApp2At[
+      MapKListOf[F,F#Out],
+      F,
       InT
     ]
   )
-  : AnyApp1At[MapKListOf[F,Y], H :: InT] { type Y = O :: mapof.Y } =
-    App1[MapKListOf[F,Y], H :: InT,  O :: mapof.Y] {
-      (s: H :: InT) => evF(s.head) :: mapof(s.tail)
+  : AnyApp2At[MapKListOf[F,F#Out], F, H :: InT] { type Y = O :: mapof.Y } =
+    App2[MapKListOf[F,F#Out], F, H :: InT, O :: mapof.Y] {
+      (f: F, s: H :: InT) => evF(s.head) :: mapof(f,s.tail)
     }
 }
