01170ff9a553d0b70c6f5b7a89ac42e3fc147cf3
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-11-19 11:43:01 +0100

    move product type project to a separate file

--- src/main/scala/cosas/types/productTypes.scala
+++ src/main/scala/cosas/types/productTypes.scala
@@ -57,39 +57,3 @@ case class :×:[H <: T#Bound, T <: AnyProductType](val head: H, val tail: T) ext
 
   lazy val label: String = s"${head.label} :×: ${tail.label}"
 }
-
-class Project[Ts <: AnyProductType, T <: AnyType] extends DepFn1[
-  AnyDenotation { type Tpe = Ts },
-  AnyDenotation { type Tpe = T }
-]
-
-case object Project extends ProjectInTail {
-
-  implicit def foundInHead[
-    H <: Ts#Types#Bound { type Raw >: V }, V,
-    Ts <: AnyProductType { type Raw >: Ds }, Ds <: AnyKList { type Bound = AnyDenotation }
-  ]
-  : AnyApp1At[
-      Project[H :×: Ts, H],
-      (H :×: Ts) := ((H := V) :: Ds)
-    ] { type Y = H := V } =
-    App1 { x: (H :×: Ts) := ((H := V) :: Ds) => x.value.head  }
-}
-
-trait ProjectInTail {
-
-  implicit def foundInTail[
-    H <: Ts#Types#Bound { type Raw >: V }, V,
-    Ts <: AnyProductType { type Raw >: Ds }, Ds <: AnyKList { type Bound = AnyDenotation },
-    P <: AnyType { type Raw >: W }, W
-  ]
-  (implicit
-    proj: AnyApp1At[Project[Ts,P], Ts := Ds] { type Y = P := W }
-  )
-  : App1[
-    Project[H :×: Ts, P],
-    (H :×: Ts) := ((H := V) :: Ds),
-    P := W
-  ] =
-    App1 { x: (H :×: Ts) := ((H := V) :: Ds) => proj( new (Ts := Ds)(x.value.tail)) }
-}
