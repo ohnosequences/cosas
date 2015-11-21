d5cf1db7bc40e104217f42c655210d66260a3911
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-11-21 20:40:02 +0100

    change Cons/Snoc to cons/snoc

--- src/main/scala/cosas/klists/cons.scala
+++ src/main/scala/cosas/klists/cons.scala
@@ -5,8 +5,8 @@ import ohnosequences.cosas.fns._
 case object Cons extends DepFn2[Any, AnyKList, AnyKList] {
 
   implicit def default[X <: L#Bound, L <: AnyKList]:
-    AnyApp2At[Cons.type, X, L] { type Y = X :: L } =
+    AnyApp2At[cons, X, L] { type Y = X :: L } =
     App2 { (x: X, l: L) => x :: l }
 }
 
-case object Snoc extends Flip[Cons.type]
+case object Snoc extends Flip[cons]
